0c22f697ab5fe8a2f650bc408e5a4b9901e633eb
Alae <aamimi454@gmail.com>
2025-11-06 20:21:44 +0000

    B5-1 #done adding possibility of saving the form of the entered event

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ buttonside.forEach(element => {
         affichage.forEach(screen => screen.classList.toggle('is-visible', screen.dataset.screen === element.dataset.screen))
     })
 })
+let theForm = document.getElementsByTagName("form")[0]
+theForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let title = document.getElementById("event-title")
+    let img = document.getElementById("event-image")
+    let description = document.getElementById("event-description")
+    let seats = document.getElementById("event-seats")
+    let price = document.getElementById("event-price")
+})
