62b3c6ecd429f3c6a138f1de5fc53c5bd40c7e1c
Alae <aamimi454@gmail.com>
2025-11-06 19:13:58 +0000

    B5-10 #done Making the naviguation possible in the siteweb

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+let events = [];
+let archive = [];
 const buttonside = document.querySelectorAll(".sidebar__btn")
+let affichage = document.querySelectorAll("section[data-screen]")
 buttonside.forEach(element => {
     element.addEventListener("click", (e) => {
         buttonside.forEach(btn => btn.classList.remove("is-active"))
         element.classList.add("is-active")
+        affichage.forEach(screen => screen.classList.toggle('is-visible', screen.dataset.screen === element.dataset.screen))
     })
 })
